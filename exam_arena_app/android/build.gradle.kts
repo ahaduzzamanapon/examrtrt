07885d30f2dc58fef24bc767f34b9ea13a8aa881
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-08-07 01:09:12 +0600

    fix: Disable Kotlin incremental compilation and enforce Java 17 across subprojects

--- exam_arena_app/android/build.gradle.kts
+++ exam_arena_app/android/build.gradle.kts
@@ -15,8 +15,23 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+
+    project.afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            android?.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
 }
 
 tasks.register<Delete>("clean") {

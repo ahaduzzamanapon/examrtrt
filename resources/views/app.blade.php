0114c15bda6a2d172107c33518cb2b12b3b98361
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-08-09 17:55:24 +0600

    fix(blade): escape curly braces in JSON-LD search_term_string template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -48,7 +48,7 @@
               "inLanguage": "bn-BD",
               "potentialAction": {
                 "@type": "SearchAction",
-                "target": "https://exam-arena.nxly.online/?s={search_term_string}",
+                "target": "https://exam-arena.nxly.online/?s={{ '{search_term_string}' }}",
                 "query-input": "required name=search_term_string"
               }
             },
